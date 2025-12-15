ea1422b301695db4f8d538c90ea357f58420340a
raihan azis <raihanangga824@gmail.com>
2025-12-15 11:50:39 +0700

    well ah

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tentang', function () {
+    // ================================================
+    // Route::get() = Tangani HTTP GET request
+    // '/tentang'   = URL yang akan dihandle
+    // function     = Kode yang dijalankan saat URL diakses
+    // ================================================
+
+    return view('tentang');
+    // ↑ return view('tentang') = Tampilkan file tentang.blade.php
+    // ↑ Laravel akan mencari di: resources/views/tentang.blade.php
+});
+
+
+ Route::get('/sapa/{nama}', function ($nama) {
+    return "Halo, $nama! Selamat datang di Toko Online Raihan.";
+});
+
+Route::get('/kategori/{nama?}', function ($nama = 'Semua') {
+    // ↑ {nama?} = Tanda ? berarti parameter OPSIONAL
+    // ↑ $nama = 'Semua' = Nilai default jika parameter tidak diberikan
+
+    return "Menampilkan kategori: $nama";
+});
